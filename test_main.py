b5d4364fec973b04607342555fc0cc0e6bdb9183
Christian Frank <cabaladifrank@gmail.com>
2026-06-04 19:06:16 -0300

    adiciona testes para tratar nome

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-from main import criar_saudacao
+from main import criar_saudacao, tratar_nome
 
 
 def test_criar_saudacao_com_nome():
@@ -11,9 +11,21 @@ def test_criar_saudacao_sem_nome():
     assert resultado == "Você não digitou um nome. Rode o programa novamente."
 
 
+def test_tratar_nome_com_espacos():
+    resultado = tratar_nome("   christian frank   ")
+    assert resultado == "Christian Frank"
+
+
+def test_tratar_nome_sem_nome():
+    resultado = tratar_nome("   ")
+    assert resultado == ""
+
+
 def main():
     test_criar_saudacao_com_nome()
     test_criar_saudacao_sem_nome()
+    test_tratar_nome_com_espacos()
+    test_tratar_nome_sem_nome()
     print("Todos os testes passaram.")
 
 
