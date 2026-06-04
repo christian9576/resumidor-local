b76c58f9ec6aca481c764f33dd8b291bf7ddad18
Christian Frank <cabaladifrank@gmail.com>
2026-06-04 18:23:24 -0300

    separa criação da saudação

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+def criar_saudacao(nome):
+    if nome == "":
+        return "Você não digitou um nome. Rode o programa novamente."
+    else:
+        return f"Olá, {nome}. Bem-vindo à sua jornada builder."
+
+
 def main():
     nome = input("Qual é o seu nome? ").strip().title()
-
-    if nome == "":
-        print("Você não digitou um nome. Rode o programa novamente.")
-    else:
-        print(f"Olá, {nome}. Bem-vindo à sua jornada builder.")
+    print(criar_saudacao(nome))
 
 
 if __name__ == "__main__":
