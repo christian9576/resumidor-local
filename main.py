8f21e09652d4584b4f7b7677e0d53b80c90d855c
Christian Frank <cabaladifrank@gmail.com>
2026-06-04 18:00:22 -0300

    organiza código em função main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-nome = input("Qual é o seu nome? ").strip().title()
+def main():
+    nome = input("Qual é o seu nome? ").strip().title()
 
-if nome == "":
-    print("Você não digitou um nome. Rode o programa novamente.")
-else:
-    print(f"Olá, {nome}. Bem-vindo à sua jornada builder.")
+    if nome == "":
+        print("Você não digitou um nome. Rode o programa novamente.")
+    else:
+        print(f"Olá, {nome}. Bem-vindo à sua jornada builder.")
+
+
+main()
